2d9dd7fb04d92804ecbd2b6b8c571c02deec98f0
MAlsultan1 <mish3l.8@hotmail.com>
2024-04-03 06:23:43 +0300

    a hangman game.

--- src/guess.dart
+++ src/guess.dart
@@ -1,5 +1,8 @@
-/* String getValidGuess() {
--The getValidGuess function prompts the player to enter a guess.
--It uses a loop to ensure that the input is a valid guess.
--It reads the input from the user, trims and converts it to lowercase, and validates it using a regular expression.
-} */
+String getValidGuess(String guess, String guess_word) {
+  if(guess_word.contains(guess)){
+    return "Correct!";
+  }
+  else{
+    return "Incorrect!";
+  }
+}
--- src/hangman.dart
+++ src/hangman.dart
@@ -1,7 +1,72 @@
 
-void hangman() {
-  print("Welcome to Hangman!");
+
 
   //List<String> words = []; Start with a list of any words you wish to add
   //choose random word from the list as the guessed word
+
+import 'dart:core';
+import 'dart:io';
+import 'dart:math';
+import 'guess.dart';
+String? guess_word;
+void hangman(){
+  Random random = new Random();
+  int randomNumber = random.nextInt(2);
+  List<String> words = ["hangman","flutter"];
+  String guess_word = words[randomNumber];
+  String answer = "_"*guess_word.length;
+  String? guess;
+  List<String> guessed_letters = [];
+  int attempts = 5;
+  print("Welcome to Hangman!");
+  while(guess_word != answer){
+    if(attempts!=0){
+      print("Word: "+answer);
+      print("Guessed letters: ");
+      for(int i=0; i<guessed_letters.length; i++){
+        print(guessed_letters[i]);
+      }
+      String str_attempts = attempts.toString();
+      print("Attempts left: "+str_attempts);
+      try{
+          print("Enter your guess (a single letter): ");
+          guess = stdin.readLineSync();
+          if(guess==""){
+            print("Write something!");
+            continue;
+          }
+          else if(guessed_letters.contains(guess)){
+            print("The letter is already used!");
+            continue;
+          }
+          else if(guess!.length>1){
+            print("Type only 1 letter!");
+            continue;
+          }
+      }
+      catch(e){
+        print("error");
+      }
+      guessed_letters.add(guess!);
+      guess = guess.trim().toLowerCase();
+      print(getValidGuess(guess,guess_word));
+      if(getValidGuess(guess,guess_word)=="Correct!"){
+        for(int i=0; i<guess_word.length;i++){
+          if(guess==guess_word[i]){
+            answer = answer.replaceFirst("_", guess, i);
+            if(answer == guess_word){
+              print("Congratulations! You guessed the word: "+guess_word);
+            }
+          }
+        }
+      }
+      else{
+        attempts = --attempts;
+      }    
+    }
+    else{
+      print("Sorry, you ran out of attempts. The correct word was: "+guess_word);
+      break; 
+    }
+  }  
 }
